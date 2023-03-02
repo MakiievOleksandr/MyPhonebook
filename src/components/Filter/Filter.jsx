920d0d55c510a362328d1a2ccdc956a64082fc1f
Makiiev Oleksandr <alsermak@gmail.com>
2023-03-02 04:24:02 +0200

    add start files

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+
+import css from '../Filter/filter.module.css';
+
+function Filter({ filter, onChangeFilter }) {
+  console.log(filter);
+  return (
+    <label className={css.label}>
+      <input
+        placeholder="Find contacts by name"
+        type="text"
+        value={filter}
+        name="filter"
+        onChange={onChangeFilter}
+        className={css.input}
+        autoComplete="off"
+      />
+    </label>
+  );
+}
+
+export default Filter;
+
+Filter.propTypes = {
+  filter: PropTypes.string.isRequired,
+  onChangeFilter: PropTypes.func.isRequired,
+};
